29aa9518a818baf573cf01b3721459d035e1c4d0
PedroArthur1981 <137727793+PedroArthur1981@users.noreply.github.com>
2024-03-27 19:14:31 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'braga_app',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
